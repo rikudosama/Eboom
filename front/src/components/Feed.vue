78fcf25a79c5dda0d59138d22bea2431495af081
lengam bonaveneture <bjean633@gmail.com>
2017-06-28 19:35:36 +0000

    Product component

--- front/src/components/Feed.vue
+++ front/src/components/Feed.vue
@@ -1,25 +1,14 @@
 <template>
 	<div>
 		<h1>Feed</h1>
-		<ul>
-			<li v-for="product in products">
-				{{ product.name }} | {{ product.price }}
-			</li>
-		</ul>
+		<my-products></my-products>
 	</div>
 </template>
 <script>
+	import Products from './product/Products.vue'
 	export default {
-		data() {
-			return {
-				products: []
-			}
-		},
-		created() {
-			this.$http.get('api/products')
-			     .then(response => {
-			     	this.products = response.body
-			     })
+		components: {
+			'my-products': Products
 		}
 	}
 </script>
---
+++ front/src/components/product/Product.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<div class="col-md-4">
+			<div class="thumbnail">
+			<img src="" alt="">
+			<div class="caption">
+				<h3>{{ product.name }}</h3>
+				<p>{{ product.price }}</p>
+				<hr>
+				<p>
+					<a href="#" class="btn btn-default">
+						Wish list
+					</a>
+					<a href="#" class="btn btn-success">
+						Buy
+					</a>
+				</p>
+			</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		props: ['product']
+	}
+</script>
